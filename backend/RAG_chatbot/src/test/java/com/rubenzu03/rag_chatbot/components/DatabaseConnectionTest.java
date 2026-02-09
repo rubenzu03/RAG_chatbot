745606d376a68aab745399ac0929d0bd72fcdb21
Rubén Zuñiga Arancon <130458067+rubenzu03@users.noreply.github.com>
2026-02-09 18:08:33 +0100

    Test for db connection

---
+++ backend/RAG_chatbot/src/test/java/com/rubenzu03/rag_chatbot/components/DatabaseConnectionTest.java
@@ -0,0 +1,32 @@
+package com.rubenzu03.rag_chatbot.components;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+public class DatabaseConnectionTest {
+
+    @Autowired
+    private DataSource dataSource;
+
+
+    @Test
+    public void testConnection() {
+        try (Connection connection = dataSource.getConnection()){
+            assertNotNull(connection);
+            assertTrue(connection.isValid(10));
+            assertFalse(connection.isClosed());
+        }
+        catch (Exception e){
+            fail(e);
+        }
+
+    }
+}
