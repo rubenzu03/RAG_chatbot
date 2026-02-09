1ad28f5fffe44cfdfaed618901f12a58697b4a45
Rubén Zuñiga Arancon <130458067+rubenzu03@users.noreply.github.com>
2026-02-09 18:08:24 +0100

    Integration with more models

--- backend/RAG_chatbot/src/main/java/com/rubenzu03/rag_chatbot/config/OllamaModels.java
+++ backend/RAG_chatbot/src/main/java/com/rubenzu03/rag_chatbot/config/OllamaModels.java
@@ -10,7 +10,8 @@ import org.springframework.context.annotation.Configuration;
 public class OllamaModels {
 
     public OllamaChatModel ollamaLlamaModel;
-
+    public OllamaChatModel ollamaCodeLlamaModel;
+    public OllamaChatModel ollamaDeepseekModel;
 
     public OllamaModels() {
         OllamaApi ollamaApi = OllamaApi.builder().build();
@@ -19,6 +20,21 @@ public class OllamaModels {
                 .defaultOptions(
                         OllamaChatOptions.builder()
                                 .model(OllamaModel.LLAMA3_1)
+                                .temperature(0.0)
+                                .build())
+                .build();
+
+        this.ollamaCodeLlamaModel = OllamaChatModel.builder()
+                .ollamaApi(ollamaApi)
+                .defaultOptions(OllamaChatOptions.builder().model(OllamaModel.CODELLAMA)
+                        .temperature(0.0).build())
+                .build();
+
+        this.ollamaDeepseekModel = OllamaChatModel.builder()
+                .ollamaApi(ollamaApi)
+                .defaultOptions(
+                        OllamaChatOptions.builder()
+                                .model("DEEPSEEK-R1")
                                 .temperature(0.0)
                                 .build())
                 .build();
