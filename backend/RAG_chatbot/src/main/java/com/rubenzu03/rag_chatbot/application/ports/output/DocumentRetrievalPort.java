194f59cfd9f0d9a8ebf7e55216b3118274a93bc5
Rubén Zuñiga Arancon <130458067+rubenzu03@users.noreply.github.com>
2026-03-29 13:48:07 +0200

    backend: remove unused port and transated querytransform prompt template

--- backend/RAG_chatbot/src/main/java/com/rubenzu03/rag_chatbot/application/ports/output/DocumentRetrievalPort.java
+++
@@ -1,8 +0,0 @@
-package com.rubenzu03.rag_chatbot.application.ports.output;
-
-import java.util.List;
-
-public interface DocumentRetrievalPort {
-    List<String> retrieveDocuments(String query);
-}
-
