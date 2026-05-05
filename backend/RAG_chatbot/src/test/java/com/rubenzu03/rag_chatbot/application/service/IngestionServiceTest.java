6938ce61fa30cfb7dcbd77b4e9c7b60dc52b468e
Rubén Zuñiga Arancon <130458067+rubenzu03@users.noreply.github.com>
2026-05-05 16:50:48 +0200

    backend tests: fixed ingestion service test

--- backend/RAG_chatbot/src/test/java/com/rubenzu03/rag_chatbot/application/service/IngestionServiceTest.java
+++ backend/RAG_chatbot/src/test/java/com/rubenzu03/rag_chatbot/application/service/IngestionServiceTest.java
@@ -9,9 +9,11 @@ import org.springframework.test.util.ReflectionTestUtils;
 
 import java.util.List;
 
+import org.mockito.Mockito;
 import static org.mockito.ArgumentMatchers.anyList;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -49,6 +51,6 @@ class IngestionServiceTest {
 
         service.ingestDocuments();
 
-        verify(vectorStore).add(anyList());
+        verify(vectorStore, times(2)).add(anyList());
     }
 }
